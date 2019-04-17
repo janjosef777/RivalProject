30334410870288d0d8e9d3d08ff87ab9e7242df7
Felix <felixgalizdo@gmail.com>
2019-04-17 11:13:15 -0700

    added a drag and drop package in CardCreation.js

--- src/components/CardCreation.js
+++ src/components/CardCreation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 
 class CardCreation extends React.Component {
     render(){
         return(
             <div className="CardCreation">
+
+                <Dropzone
+                    multiple={false}
+                    accept="image/*"
+                    onDrop={this.onImageDrop.bind(this)}>
+                    <p>Drop an image or click to select a file to upload.</p>
+                </Dropzone>
+
             </div>
         )
     }
