39ef8dbe05080b4744c67fc1e1e890a02177f5c5
Felix <felixgalizdo@gmail.com>
2019-04-17 10:24:20 -0700

    placed bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
-import '../sass/App.scss';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 
 class Login extends React.Component {
 
     render(){
         return(
-            <div className="loginPage">
-                <tbody className="loginContainer">
-                    <tr>
-                        <th></th>
-                        <th>HELLO</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <td><p>Username: </p></td>
-                        <td><input type="text"/></td>
-                        <td></td>
-                    </tr>
-                    <tr> 
-                        <td><p>Password: </p></td>
-                        <td><input type="text"/></td>
-                        <td></td>
-                    </tr> 
-                    <tr>
-                        <td></td>
-                        <td><input type="button" value="LOGIN" class="btn btn-success"/></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </div>
+            <Form>
+                <FormGroup>
+                  <Label for="username">Username</Label>
+                  <Input type="text" name="username" id="usernameInput" placeholder="Username" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                </FormGroup>  
+            </Form>
+            
         )
     }
 }
