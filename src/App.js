e4cd3de58b05ade7b9de3523ab1ab2b1e7222980
daryashokouhi00 <daryashokouhi00@gmail.com>
2019-04-17 11:37:29 -0700

    Upload file function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardCreation from './components/CardCreation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardCreation />
       </div>
     );
   }
--- src/components/CardCreation.js
+++ src/components/CardCreation.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Upload from '../upload/Upload';
 
 class CardCreation extends React.Component {
     render(){
         return(
             <div className="CardCreation">
+                <Upload />
             </div>
         )
     }
