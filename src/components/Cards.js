14b5e9f07cf4511d40a80d43baedd1bf959333d0
daryashokouhi00 <daryashokouhi00@gmail.com>
2019-04-18 13:55:16 -0700

    Some stuff I did

---
+++ src/components/Cards.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Upload from '../upload/Upload';
+import { Card, Button, CardTitle, CardText } from 'reactstrap'; 
+
+
+class Cards extends React.Component {
+    constructor(props) {
+        super(props);
+        const cards = [1, 2, 3, 4, 5];
+        this.listItems = cards.map((card) =>
+            <li>{card}</li>
+        );
+    }
+
+
+    render(){
+        return(
+            <div>
+                <ul>{this.listItems}</ul>
+            </div>
+        )
+    }
+}
+
+export default Cards;
