230970b40da2a55548d89453d993895175d15d40
une-s <36519297+une-s@users.noreply.github.com>
2019-04-20 10:38:10 -0700

    Delete image files when done with them

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,6 +1,7 @@
 const sharp = require('sharp');
 const fnHelper = require('../io/filename-helper');
 const mkdirp = require('mkdirp');
+const fs = require('fs');
 const db = require('../db');
 const uploadUrl = '/uploads/'; // Start with and end with a slash
 
@@ -32,10 +33,12 @@ module.exports = {
     },
     // post
     post: (req, res, next) => {
+        const tempPath = req.file.path;
         if(!/^image\b/.test(req.file.mimetype)) {
+            fs.unlink(tempPath, handleErr);
             res.status(400).send('Not an image!');
         } else {
-            const image = sharp(req.file.path);
+            const image = sharp(tempPath);
             image.metadata().then(metadata => {
 
                 const dir = 'public' + uploadUrl;
@@ -44,9 +47,12 @@ module.exports = {
                     metadata.format);
 
                 mkdirp(dir, err => {
-                    if(err)
+                    if(err) {
+                        fs.unlink(tempPath, handleErr);
                         return handleErr(err, res, 500);
+                    }
                     image.toFile(dir + filename, (err, savedImg) => {
+                        fs.unlink(tempPath, handleErr);
                         if(err)
                             return handleErr(err, res, 500);
                         const jsonImage = {
@@ -75,18 +81,33 @@ module.exports = {
         const id = +req.params.id;
         if(!(id > 0))
             return handleErr(null, res, 404);
-        db.images.delete(id, (err, success) => {
+
+        // Get image details
+        db.images.get(id, (err, image) => {
             if(err)
                 return handleErr(err, res, 500);
-            if(!success)
+            if(!image)
                 return handleErr(null, res, 404);
-            res.send({ id: id, deleted: success });
-        })
+
+            // Delete image file
+            fs.unlink('public' + uploadUrl + image.filename, err => {
+                if(err)
+                    return handleErr(err, res, 500);
+                
+                // Delete image from db
+                db.images.delete(id, (err, success) => {
+                    if(err)
+                        return handleErr(err, res, 500);
+                    res.send({ id: id, deleted: success });
+                });
+            })
+        });
     }
 };
 
 function handleErr(err, res, status, message) {
     if(err)
         console.log(err);
-    res.status(status).send(message);
+    if(res)
+        res.status(status).send(message);
 }
