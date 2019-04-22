b5592642bb0b69d462db1d34ad005bb67e7aaa6f
jmiranda9 <jmiranda9@my.bcit.ca>
2019-04-22 13:32:27 -0700

    Authentication working/ token set in localstorage

--- src/EnsureLoggedIn.js
+++ src/EnsureLoggedIn.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-
 class EnsureLoggedIn extends React.Component {
-    isLoggedIn = true;
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false
+        };
+        this.checkToken = this.checkToken.bind(this)
+    }
+    checkToken() {
+        var legit = window.localStorage.getItem('token')
+        console.log(legit);
+        if (legit !== null) {
+            this.setState({
+                isLoggedIn: true
+            }, () => {
+                console.log(this.state.isLoggedIn)
+            })
+        } else {
+            this.setState({
+                isLoggedIn: false
+            }, () => {
+                console.log(this.state.isLoggedIn)
+            })
+        }
+    }
 
+    componentDidMount() {
+        this.checkToken();
+    }
     render() {
-        if (this.isLoggedIn) {
+        if (this.state.isLoggedIn) {
             return this.props.children
         } else {
             return null
--- src/components/Login.js
+++ src/components/Login.js
@@ -15,48 +15,48 @@ class Login extends Component {
         this.handleUsername = this.handleUsername.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-      }
+    }
 
-      handleSubmit() {
+    handleSubmit() {
         fetch('http://localhost:4000/api/auth', {
             method: 'POST',
-            body: JSON.stringify({"username" : this.state.username, "password": this.state.password}),
+            body: JSON.stringify({ "username": this.state.username, "password": this.state.password }),
             headers: {
                 'Content-Type': 'application/json'
             }
         }).then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                
-                console.log(response);
-                return response;
-                window.location.reload();
-              } else {
-               console.log('Somthing happened wrong');
-              }
-        }).catch(err => err);
+            response.json()
+                .then(responseJson => {
+                    console.log(JSON.stringify(responseJson));
+                    window.localStorage.setItem("token", JSON.stringify(responseJson));
+                    window.setTimeout(function () {
+                        window.location.href = "/";
+                    }, 2000);
+                })
+        })
     }
 
     handleUsername(event) {
-        this.setState({username: event.target.value});
-      }
-    
-      handlePassword(event) {
-        this.setState({password: event.target.value});
-      }
+        this.setState({ username: event.target.value });
+    }
 
-    render(){
-        return(
-            <div className="loginPage">
-                <img src={logo} className="logoImage"/>
+    handlePassword(event) {
+        this.setState({ password: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="loginPage" >
+                <img src={logo} className="logoImage" />
                 <Form>
                     <FormGroup>
-                      <Label for="username">Username</Label>
-                      <Input type="text" id="username" value={this.state.username} onChange={this.handleUsername} placeholder="Username" name="username" />
+                        <Label for="username">Username</Label>
+                        <Input type="text" id="username" value={this.state.username} onChange={this.handleUsername} placeholder="Username" name="username" />
                     </FormGroup>
                     <FormGroup>
-                      <Label for="password">Password</Label>
-                      <Input type="password" value={this.state.password} onChange={this.handlePassword} name="password" id="password" placeholder="Password" />
-                    </FormGroup>  
+                        <Label for="password">Password</Label>
+                        <Input type="password" value={this.state.password} onChange={this.handlePassword} name="password" id="password" placeholder="Password" />
+                    </FormGroup>
                     <Button onClick={this.handleSubmit}>Login</Button>
                 </Form>
             </div>
