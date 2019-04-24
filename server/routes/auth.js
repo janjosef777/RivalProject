1fc03716ab5925c9933db23eeba3c1db33d4c336
une-s <36519297+une-s@users.noreply.github.com>
2019-04-23 19:37:29 -0700

    Bug fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,14 +11,14 @@ module.exports = {
         function sendToken(token) {
             res.json({ "token": token })
         }
-        function userVerified(req, res) {
-            if (res === false) {
+        function userVerified(req, username) {
+            if (username === false) {
                 console.log("Incorrect username/password");
                 res.status(403).send("Incorrect username/password");
             } else {
-                console.log(res);
+                console.log(username);
                 //issue token 
-                const payload = { username: res };
+                const payload = { username: username };
                 const secret = process.env.SECRET;
                 const token = jwt.sign(payload, secret, {
                     expiresIn: '1h'
