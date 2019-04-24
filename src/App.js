a33fd8400fd540d64d1aa567ad5ef3bf419bd6c4
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-04-23 20:54:39 -0700

    manditory

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ const App = () => (
         <NavbarBrand>Rival Scratch Reward Manager</NavbarBrand>
         <Nav className="ml-auto" navbar>
           
+          <NavItem><Link to="/login">Login</Link></NavItem>
           <EnsureLoggedIn>
-            <NavItem><Link to="/login">Login</Link></NavItem>
             <NavItem><Link to="/">Home</Link></NavItem>
             <NavItem><Link to="/cardcreation">Card Creation</Link></NavItem>
             <NavItem><Link to="/finalcreation">Final Creation</Link></NavItem>
