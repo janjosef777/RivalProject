4bfc193891083e1d4786eca8e4c4bdedb7153d84
une-s <36519297+une-s@users.noreply.github.com>
2019-04-23 19:26:29 -0700

    Enable get all card results with given campaign id

--- server/db/cardResults.js
+++ server/db/cardResults.js
@@ -2,15 +2,27 @@ module.exports = function(con, db) {
 
     const queries = {
         getAll: 'SELECT * FROM card_result',
+        getAllInCampaign: 'SELECT * FROM card_result WHERE campaign=?',
         get: 'SELECT * FROM card_result WHERE id=?',
         add: 'INSERT INTO card_result (title, image, campaign, prize, quantity) VALUES (?,?,?,?,?)',
         delete: 'DELETE FROM card_result WHERE id=?',
     };
     return {
-        getAll(callback) {
-            db.onReady = () => con.query(queries.getAll, (err, res) => {
-                callback(err, err ? [] : res);
-            });
+        /**
+         * Possible parameters:
+         * getAll(callback) - gets all
+         * getAll(campaignId, callback) - gets all in campaign
+         */
+        getAll() {
+            let campaignId = arguments.length >= 2 ? arguments[0] : null;
+            let callback   = arguments.length >= 2 ? arguments[1] : arguments[0];
+            db.onReady = campaignId ?
+                () => con.query(queries.getAll, campaignId, (err, res) => {
+                    callback(err, err ? [] : res);
+                }) :
+                () => con.query(queries.getAll, (err, res) => {
+                    callback(err, err ? [] : res);
+                });
         },
         get(id, callback) {
             db.onReady = () => con.query(queries.get, id, (err, res) => {
