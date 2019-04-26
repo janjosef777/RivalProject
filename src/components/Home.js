1e554c6e9cbbddfa06bddb3cb70af1e85ef08d01
Felix <felixgalizdo@gmail.com>
2019-04-26 15:02:58 -0700

    installed  a crud table library

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { Table, Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
 import '../styles/home.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-import CRUDTable,
-{
-  Fields,
-  Field,
-  CreateForm,
-  UpdateForm,
-  DeleteForm,
-  Pagination
-} from 'react-crud-table';
+import Paper from '@material-ui/core/Paper';
+
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  Button
+} from '@devexpress/dx-react-grid-material-ui';
 
 let campaignItems = [];
 let count = campaignItems.length;
@@ -89,22 +86,46 @@ class Home extends Component {
 
     constructor(props){
         super(props);
-        this.state={
 
-        }
-    } 
+        this.state = {
+            columns: [
+              { name: 'name', title: 'Name' },
+              { name: 'sex', title: 'Sex' },
+              { name: 'city', title: 'City' },
+              { name: 'car', title: 'Car' }
+            ],
+            rows: [
+              { sex: "Female", name: "Sandra", city: "Las Vegas", car: "Audi A4" },
+              { sex: "Male", name: "Paul", city: "Paris", car: "Nissan Altima" },
+              { sex: "Male", name: "Mark", city: "Paris", car: "Honda Accord" },
+              { sex: "Male", name: "Paul", city: "Paris", car: "Nissan Altima" },
+              { sex: "Female", name: "Linda", city: "Austin", car: "Toyota Corolla" },
+              { sex: "Male", name: "Robert", city: "Las Vegas", car: "Chevrolet Cruze" },
+              { sex: "Female", name: "Lisa", city: "London", car: "BMW 750" },
+              { sex: "Male", name: "Mark", city: "Chicago", car: "Toyota Corolla" },
+              { sex: "Male", name: "Thomas", city: "Rio de Janeiro", car: "Honda Accord" },
+              { sex: "Male", name: "Robert", city: "Las Vegas", car: "Honda Civic" },
+              { sex: "Female", name: "Betty", city: "Paris", car: "Honda Civic" },
+              { sex: "Male", name: "Robert", city: "Los Angeles", car: "Honda Accord" },
+              { sex: "Male", name: "William", city: "Los Angeles", car: "Honda Civic" },
+              { sex: "Male", name: "Mark", city: "Austin", car: "Nissan Altima" }
+            ]
+          };
+    }
 
     render(){
-        return(
-            <div className="Home">
-        
-                <div className="container">
-                <Table>
-                    
-                </Table>
-                </div>
-            </div>
-        )
+        const { rows, columns } = this.state;
+        return (
+        <Paper>
+            <Grid
+            rows={rows}
+            columns={columns}
+            >
+                <Table />
+                <TableHeaderRow />
+            </Grid>
+        </Paper>
+          );
     }
 }
 
