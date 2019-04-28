dce950ad7515e995e141ba3d21b86ff1ba88cd55
jmiranda9 <jmiranda9@my.bcit.ca>
2019-04-27 22:35:59 -0700

    rebasing

--- server/db/images.js
+++
@@ -1,8 +0,0 @@
-const tableName = 'image';
-const columns = ['filename', 'width', 'height'];
-
-module.exports = require('./crudBase').create(tableName, columns, {
-    mapRead: image => Object.assign(image, {
-        path: '/uploads/' + image.filename
-    })
-});
--- server/db/prizes.js
+++
@@ -1,4 +0,0 @@
-const tableName = 'prize';
-const columns = ['name', 'value', 'quantity'];
-
-module.exports = require('./crudBase').create(tableName, columns);
