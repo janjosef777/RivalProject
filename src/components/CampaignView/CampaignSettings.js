8d7e376cada910dc817ab19ea93a945fee90fa8f
Jan Miranda <A01037768@ad.bcit.ca>
2019-04-29 14:52:35 -0700

    Update CampaignSettings.js

--- src/components/CampaignView/CampaignSettings.js
+++ src/components/CampaignView/CampaignSettings.js
@@ -20,6 +20,7 @@ class CampaignSettings extends Component {
 
     render() {
         return (
+            
             <div className='wrapper'>
                 <h6>Campaign Name: {this.props.selectedCampaign.name} </h6>
                 <h6>Campaign Reach: {this.props.selectedCampaign.estimatedParticipants}</h6>
@@ -28,7 +29,28 @@ class CampaignSettings extends Component {
                     <input type="checkbox" defaultChecked />
                     <span className="slider round"></span>
                 </label>
+                <div className='wrapper'>
+                <div className="input-section">        
+                    <h6>Campaign Name: </h6>
+                    <input type="text" placeholder="Campaign Name" />
+                </div>
+                <div className="input-section">
+                    <h6>Estimated Participants: </h6>
+                    <input type="text" placeholder="Campaign Reach" />
+                </div>
+                <div className="input-section">
+                    <h6>Campaign Activation Status: </h6>
+                    <label class="switch">
+                    <input type="checkbox" checked />
+                    <span className="slider round"></span>
+                    </label>
+                </div>
+                <div className="input-section">
+                    <h6>Generated URL: </h6>
+                </div>
             </div>
+            </div>
+            
 
         )
     }
