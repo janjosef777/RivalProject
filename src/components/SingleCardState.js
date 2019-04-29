009d854423547cda2d1c65943ddb430cc8a9ea2a
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-04-28 18:34:30 -0700

    styled the clickable card

--- src/components/SingleCardState.js
+++ src/components/SingleCardState.js
@@ -1,4 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CardFrame = styled.div`
+    width: ${props => props.frameWidth + 'px'};
+    padding-bottom: ${props => props.framePadding + 'px'};
+    background-color: ${props => props.frameColor};
+    border: ${props => props.borderStyle};
+`;
+
+const Title = styled.h1`
+    font-size: 1.2em;
+    text-align: center;
+    padding: ${props => props.framePadding + 'px'}
+    border-bottom: ${props => props.borderStyle};
+`;
+
+const Image = styled.img`
+    object-fit: cover;
+    display: block;
+    margin: 0 auto;
+    margin-top: ${props => props.framePadding + 'px'}
+    border: ${props => props.borderStyle};
+`;
+
 
 
 class SingleCardState extends React.Component {
@@ -7,21 +31,30 @@ class SingleCardState extends React.Component {
     }
 
     render() {
+        const framePadding = 25;
+        const frameColor = 'lightgray';
+        const borderStyle = 'solid black 1px';
         return (
-            <div>
-                <header>
-                    <h1>{this.props.title}</h1>
-                </header>
-                <main>
-                    <img
-                        width={this.props.imgWidth}
-                        height={this.props.imgHeight}
-                        src={this.props.imgSrc}
-                        alt={this.props.imgAlt}
-                        onClick={e => this.props.handleCleared(e)}
-                    />
-                </main>
-            </div>
+            <CardFrame 
+                frameWidth={this.props.imgWidth + 2 * framePadding}
+                framePadding={framePadding}
+                frameColor={frameColor}
+                borderStyle={borderStyle}
+            >
+                <Title 
+                    framePadding={framePadding}
+                    borderStyle={borderStyle}
+                >{this.props.title}</Title>
+                <Image
+                    width={this.props.imgWidth}
+                    height={this.props.imgHeight}
+                    src={this.props.imgSrc}
+                    alt={this.props.imgAlt}
+                    onClick={e => this.props.handleCleared(e)}
+                    framePadding={framePadding}
+                    borderStyle={borderStyle}
+                />
+            </CardFrame>
         )
     }
 }
