7e1dcd6553cfa45dfdf6a8353ec72fe1c1ccc3b6
daryashokouhi <darya.shokohi@ymail.com>
2019-04-30 11:28:16 -0700

    Some styling added

--- src/CardCreatorApp.js
+++ src/CardCreatorApp.js
@@ -10,6 +10,7 @@ import CampaignView from './components/CampaignView/index';
 import CreateCampaign from './components/CampaignCrud/CreateCampaign';
 import DeleteCampaign from './components/CampaignCrud/DeleteCampaign';
 import logo from '../src/images/Rivallogo.png';
+import arrow from '../src/images/arrow.png';
 import {
     Collapse,
     Navbar,
@@ -28,29 +29,36 @@ import './App.css';
 
 
 const CardCreatorApp = () => (
-    <Router>
-            <Navbar>
-                <NavbarBrand><img src={logo} className="nav-logo"/></NavbarBrand>
-                <Nav navbar>
+    <div>
+        <Router>
+                <Navbar>
+                    <NavbarBrand><img src={logo} className="nav-logo"/></NavbarBrand>
+                    <Nav navbar>
 
-                    <button className="primary-button"><Link to="/login">Login</Link></button>
+                        <button className="primary-button"><Link to="/login">Login</Link></button>
+                        <EnsureLoggedIn>
+                            <NavItem className="SingleLink"><Link to="/">Home</Link></NavItem>
+                            <NavItem className="SingleLink"><Link to="campaignview">Campaign View</Link></NavItem>
+                            <NavItem className="SingleLink"><Link to="/logout">LogOut</Link></NavItem>
+                        </EnsureLoggedIn>
+                    </Nav>
+                </Navbar>
+                <Switch>
+                    <Route exact path="/login" component={Login} />
                     <EnsureLoggedIn>
-                        <NavItem className="SingleLink"><Link to="/">Home</Link></NavItem>
-                        <NavItem className="SingleLink"><Link to="campaignview">Campaign View</Link></NavItem>
-                        <NavItem className="SingleLink"><Link to="/logout">LogOut</Link></NavItem>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/CreateCampaign" component={CreateCampaign} />
+                        <Route exact path="/DeleteCampaign" component={DeleteCampaign} />
+                        <Route exact path="/campaignview" component={CampaignView} />
+                        <Route exact path="/logout" component={Logout} />
                     </EnsureLoggedIn>
-                </Nav>
-            </Navbar>
-            <Switch>
-                <Route exact path="/login" component={Login} />
-                <EnsureLoggedIn>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/CreateCampaign" component={CreateCampaign} />
-                    <Route exact path="/DeleteCampaign" component={DeleteCampaign} />
-                    <Route exact path="/campaignview" component={CampaignView} />
-                    <Route exact path="/logout" component={Logout} />
-                </EnsureLoggedIn>
-            </Switch>
-    </Router>
+                </Switch>
+        </Router>
+        <div className="landingpage-wrapper">
+            <h1>Welcome to your scratch & win card manager</h1>
+            <h3>Login and get started with creating campaigns</h3>
+        </div>
+        <img src={arrow} className="home-arrow"/>
+    </div>
 )
 export default CardCreatorApp;
