4bcd573b13140715a4cda98115621b2b5887e64c
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-04-29 23:15:30 -0700

    done refactoring the navigation and navbar for now

--- src/components/CampaignView/index.js
+++ src/components/CampaignView/index.js
@@ -10,6 +10,7 @@ import CardTemplateView from './CardTemplateView';
 import TabView from './TabView';
 import { Link } from '@material-ui/core';
 import styled from 'styled-components'
+import NavBarComponent from '../NavBarComponent'
 
 const LinkButton = styled.a`
     padding: 10px;   
@@ -47,16 +48,17 @@ class CampaignView extends Component {
 
     render() {
         return (
-            <div className='content-wrapper'>
-                <div className='left-wrapper sub-wrapper'>
-                    <AssetsView {...this.state} setState={this.setState} ></AssetsView>
+            <div>
+                <NavBarComponent />
+                <div className='content-wrapper'>
+                    <div className='left-wrapper sub-wrapper'>
+                        <AssetsView {...this.state} setState={this.setState} ></AssetsView>
+                    </div>
+                    <div className='right-wrapper sub-wrapper'>
+                        <TabView {...this.state} setState={this.setState}></TabView>
+                        <LinkButton href="http://localhost:4000/api/assignlink/par/1/camp/1">Demo</LinkButton>
+                    </div>
                 </div>
-                <div className='right-wrapper sub-wrapper'>
-                    <TabView {...this.state} setState={this.setState}></TabView>
-                    <LinkButton href="http://localhost:4000/api/assignlink/par/1/camp/1">Demo</LinkButton>
-                </div>
-                
-
             </div>
 
         )
