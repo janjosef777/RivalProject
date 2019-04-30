5e0480266783fa37b6ecd24f542d58466b3f6048
janjosef777 <janjosef777@yahoo.com>
2019-04-30 11:45:47 -0700

    template api calls added

---
+++ server/routes/templates.js
@@ -0,0 +1,44 @@
+const db = require('../db');
+
+module.exports = {
+    get: (req, res, next) => {
+        console.log("entered get")
+        const id = req.body.id || 0;
+        console.log(id)
+
+        if (id <= 0)
+            return handleErr(null, res, 404);
+        db.templates.get(id, (err, template) => {
+            if (err)
+                return handleErr(err, res, 500);
+            if (!template)
+                return handleErr(null, res, 404);
+            res.json({
+                token: res.jwtToken,
+                data: template
+            });
+        });
+    },
+    post: (req, res, next) => {
+        const template = req.body
+        console.log(template);
+        db.templates.add(template, (err,id) => {
+            if (err)
+                return handleErr(err,res,500);
+            const data = {
+                id: id
+            };
+            res.json({
+                token: res.jwtToken,
+                data:data
+            })
+        })
+    }
+};
+function handleErr(err, res, status, message) {
+    if (err)
+        console.log(err);
+    if (res)
+        res.status(status).send(message);
+}
+
