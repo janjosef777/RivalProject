9b885138e10942a3fc6f0cd3d8c49f813a9688d8
une-s <36519297+une-s@users.noreply.github.com>
2019-05-06 16:18:01 -0700

    Remove comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -50,9 +50,6 @@ const getRowId = row => row.id;
 
 const DateFormatter = ({ value }) =>
     new Date(value).toLocaleString();
-    // value.replace(/(\d{4})-(\d{2})-(\d{2})/, '$3/$2/$1')
-    //     .replace(/T/, ' - ')
-    //     .replace(/\..+/, '');
 const DateTypeProvider = props => (
     <DataTypeProvider
         formatterComponent={DateFormatter}
