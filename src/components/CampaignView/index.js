a886d159adae0f7d455d67fd2e1fd9280c624c08
Une Simonsen <36519297+une-s@users.noreply.github.com>
2019-05-07 17:04:41 -0700

    Tidy up some react states

--- src/components/CampaignView/index.js
+++ src/components/CampaignView/index.js
@@ -39,17 +39,11 @@ class CampaignView extends Component {
             selectedIndex: null,
             viewSummary: false,
 
-            selectedCampaign_id: this.props.location.state ? this.props.location.state.selectedCampaignId : 0,
-            selectedCampaign_estimatedParticipants: 0,
-            selectedCampaign_isActive: false,
-            selectedCampaign_name: null,
-            selectedCampaign_template: null,
-            selectedCampaign_url: null,
-
-            selectedTemplate_title: null,
-            selectedTemplate_image: null,
-            selectedTemplate_imageId: null,
-
+            selectedCampaign: {
+                id: this.props.location.state ? this.props.location.state.selectedCampaignId : 0
+            },
+            selectedOverlay: null,
+            selectedOverlayImage: null
         }
         this.setState = this.setState.bind(this);
         // this.saveChanges = this.saveChanges.bind(this);
@@ -57,7 +51,7 @@ class CampaignView extends Component {
 
 
     loadCampaign() {
-        ApiHelper.fetch('http://localhost:4000/api/campaigns/' + this.state.selectedCampaign_id, {
+        ApiHelper.fetch('http://localhost:4000/api/campaigns/' + this.state.selectedCampaign.id, {
             method:
                 'GET',
             headers: {
@@ -65,83 +59,37 @@ class CampaignView extends Component {
                 "Content-type": "application/json"
             }
         }).then(res => {
-                this.setState({
-                    selectedCampaign_id: res.id,
-                    selectedCampaign_estimatedParticipants: res.estimatedParticipants,
-                    selectedCampaign_isActive: res.isActive,
-                    selectedCampaign_name: res.name,
-                    selectedCampaign_template: res.template,
-                    selectedCampaign_url: res.url,
-                })
-                this.loadTemplate()
-            })
-            .catch(err => {
-                console.error(err);
-            })
-    }
-    loadTemplate() {
-        fetch('http://localhost:4000/api/template/' + this.state.selectedCampaign_template, {
-            method:
-                'GET',
-            headers: {
-                "Authorization": "Bearer " + sessionStorage.getItem("token"),
-                "Content-type": "application/json"
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-                sessionStorage.setItem('token', res.token)
-                this.setState({
-                    selectedTemplate_imageId: res.data.image
+            const campaign = res;
+            const overlay = campaign.template;
+            const overlayImage = overlay.image;
+            delete overlay.image;
+            delete campaign.template;
 
-                })
-                if (res.data.title != null) {
-                    this.setState({
-                        selectedTemplate_title: res.data.title
-                    })
-                }
-                this.loadTemplateImage()
+            this.setState({
+                selectedCampaign: campaign,
+                selectedOverlay: overlay,
+                selectedOverlayImage: overlayImage
             })
-            .catch(err => {
-                console.error(err);
-            })
-    }
-
-    loadTemplateImage() {
-        ApiHelper.fetch('http://localhost:4000/api/images/' + this.state.selectedTemplate_imageId, {
-            method:
-                'GET',
-            headers: {
-                "Authorization": "Bearer " + sessionStorage.getItem("token"),
-                "Content-type": "application/json"
-            }
         })
-            .then(res => {
-                sessionStorage.setItem('token', res.token)
-                if (res.path != null) {
-                    this.setState({
-                        selectedTemplate_image: res.path
-                    })
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            })
+        .catch(err => {
+            console.error(err);
+        })
     }
 
     saveChanges() {
-        fetch('http://localhost:4000/api/campaigns/' + this.props.selectedCampaign_id, {
+        const campaign = this.props.selectedCampaign;
+        const overlay = this.props.selectedOverlay;
+        const overlayImage = this.props.selectedOverlayImage;
+        overlay.image = overlayImage;
+        campaign.template = overlay;
+        fetch('http://localhost:4000/api/campaigns/' + this.props.selectedCampaign.id, {
             method:
                 'PATCH',
             headers: {
                 "Authorization": "Bearer " + sessionStorage.getItem("token"),
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({
-                name: this.props.selectedCampaign_name,
-                isActive: !!this.props.selectedCampaign_isActive,
-                template: this.props.selectedCampaign_template
-            })
+            body: JSON.stringify(campaign)
         }).then(res => {
             if(res.status !== 200)
                 throw res;
