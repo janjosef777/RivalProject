1ac22dacb0d1d908dddb6b37e73655eb3d1315c8
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-05-08 10:25:16 -0700

    styling fix

--- src/components/CampaignView/CampaignSettings.js
+++ src/components/CampaignView/CampaignSettings.js
@@ -7,11 +7,6 @@ import ApiHelper from '../../helpers/ApiHelper';
 
 const LinkButton = styled.a`
 padding: 10px;   
-margin: 10px;
-margin-top: 50px;
-border: 1px solid black;
-border-radius: 5px;
-background-color: #E8542A;
 `;
 
 class CampaignSettings extends Component {
